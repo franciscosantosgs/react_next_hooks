71fba662885fe88dc0b6248b10fc5a9f94865ea7
franciscosantosgs-admin <francisco.santos@germanodesousa.com>
2025-01-20 16:31:48 +0000

    03_hooks - utilizção do useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 //import HookState01 from './components/UseState_01';
-import HookState02 from './components/UseState_02';
+//import HookState02 from './components/UseState_02';
+import { useState } from 'react';
+import HookState03 from './components/UseState_03';
 
 export default () => {
     return (
         <>
              <h1>REACT HOOKS</h1>
             {/*<HookState01 />*/}    
-            <HookState02 />    
+            {/*<HookState02 />*/}   
+            <HookState03 />
         </>
     );
 };
